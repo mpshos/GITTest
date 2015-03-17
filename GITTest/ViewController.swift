9660e6ef5ed666709be1ac46087b6cd82bcea68a
mpshos <Mike@mwc-094044.dhcp.missouri.edu>
2015-03-17 12:38:00 -0500

    Initial Commit

---
+++ GITTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GITTest
+//
+//  Created by Mike on 3/17/15.
+//  Copyright (c) 2015 Michael P Shostrand. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
